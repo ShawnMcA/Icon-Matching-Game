16af8ee15204104e0db370501b8d058adbf4115b
ShawnMcA <56180982+ShawnMcA@users.noreply.github.com>
2020-03-15 23:22:12 -0700

    Revamped the board logic
    Added a board array that stores the icons for each play. Updated the add/remove icon logic.

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+// ========================================
+// Game Icons from https://game-icons.net/
+//
+// ========================================
+
+
 const GameGrid = {
     
     // cell items
@@ -15,29 +21,70 @@ const GameGrid = {
     // game board
     gameBG: "game-background",
     cells: "game-cell",
+    board: [],
+
+    // fills the board array with icons 
+    createBoardItems: function() {
+        for(let i = 0; i < 16; i++){
+            let count = 0;
+            let item = this.randomItem();
+
+            // check for duplicates
+            for(let j = 0; j <= i; j++) {
+                if (this.board[j] === item) count++;
+            }
+
+            // fills non-duplicate
+            if (count < 2) {
+                this.board[i] = item;
+            } else i--; // reset
+        }
+    },
+
+    // fills cells with board array icons
+    fillBoardItems: function() {
+        const cells = document.getElementsByClassName("game-cell");
+
+        this.createBoardItems(); // fills board array with random numbers
+
+        for(let i = 0; i < cells.length; i++) {
+            cells[i].classList.add(this.items[this.board[i]]); // Adds items class to board cells
+        }
+    },
 
     // random Number - Cycle through cell items
-    randomNum : function () {
+    randomItem: function () {
         return Math.floor(Math.random() * 8);
     },
 
     // update cell w/icon
-    addCellItem: function (cell, icon) {
+    addCellItem: function (cell) {
+        let num = Math.abs(+cell.id.slice(cell.id.length - 2));
+        let icon = this.items[this.board[num - 1]];
         cell.classList.add(icon);
+
     },
 
     // reset cell
-    removeCellItem: function (cell, icon) {
-        cell.classList.remove(icon);
+    removeCellItem: function (cell) {
+        for(let i = 0; i < cell.classList.length; i++) {
+            let item = cell.classList.item(i);
+            if(item !== "game-cell") cell.classList.remove(item);
+        }
     },
 
-    // !!TODO!! - UPDATE EMPTY CELL LOGIC
     // empty - update cell // !empty - reset cell
     cellEmptyCheck: function(event){
-        let cellItem = GameGrid.randomNum(); // TESTING
         const cell = event.target; // click target
-        if(!cell.classList.contains(GameGrid.cells)) return; // returns if ! game cell
-        cell.classList.contains(GameGrid.items[cellItem]) ? GameGrid.removeCellItem(cell, GameGrid.items[cellItem]) : GameGrid.addCellItem(cell, GameGrid.items[cellItem]); // add game item 
+        let cellClasses = event.target.classList;
+
+        if(!cellClasses.contains(GameGrid.cells)) return; // returns if ! game cell
+
+        if(cellClasses.length > 1)  {
+            GameGrid.removeCellItem(cell);
+         } else {
+            GameGrid.addCellItem(cell); // add game item 
+         } 
     },
     
     // add grid listeners
@@ -53,12 +100,13 @@ const GameGrid = {
     }
 };
 
-// load
+// window load
 window.addEventListener("load", function(){
+    GameGrid.fillBoardItems();
     GameGrid.createGridEventListeners();
 });
 
-// unload
+// window unload
 window.addEventListener("unload", function(){
     GameGrid.deleteGridEventListeners();
 });
