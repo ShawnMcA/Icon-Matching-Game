7f56e8009deedae1f7081091d757e45a27b96fb3
ShawnMcA <56180982+ShawnMcA@users.noreply.github.com>
2020-03-15 19:21:03 -0700

    updated images

--- main.js
+++ main.js
@@ -2,12 +2,24 @@ const GameGrid = {
     
     // cell items
     items: [
-        "sword"
+        "anubis",
+        "centaur",
+        "dinosaur",
+        "ghost",
+        "hydra", 
+        "loch-ness",
+        "moon",
+        "reaper"
     ],
     
     // game board
     gameBG: "game-background",
     cells: "game-cell",
+
+    // random Number   
+    randomNum : function () {
+        return Math.floor(Math.random() * 8);
+    },
 
     // update cell w/icon
     addCellItem: function (cell, icon) {
@@ -21,9 +33,10 @@ const GameGrid = {
 
     // empty - update cell // !empty - reset cell
     cellEmptyCheck: function(event){
+        let cellItem = GameGrid.randomNum();
         const cell = event.target; // click target
         if(!cell.classList.contains(GameGrid.cells)) return; // returns if ! game cell
-        cell.classList.contains(GameGrid.items[0]) ? GameGrid.removeCellItem(cell, GameGrid.items[0]) : GameGrid.addCellItem(cell, GameGrid.items[0]); // add game item 
+        cell.classList.contains(GameGrid.items[cellItem]) ? GameGrid.removeCellItem(cell, GameGrid.items[cellItem]) : GameGrid.addCellItem(cell, GameGrid.items[cellItem]); // add game item 
     },
     
     // add grid listeners
